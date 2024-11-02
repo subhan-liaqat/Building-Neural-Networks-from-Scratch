e6bd0d4c36a2f6d9f87890e5292cf0b0dbefee47
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-11-03 00:03:00 +0500

    Create 1.Coding-a-Neuron.py

---
+++ 1.Coding-a-Neuron.py
@@ -0,0 +1,9 @@
+#Input features
+inputs = [1.2, 5.2, 2.1]
+#Weights
+weights = [3.1, 2.1, 8.7]
+#Neuron Bias
+bias = 3
+#Neuron output
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output)
